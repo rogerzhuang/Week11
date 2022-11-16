c08805dbb1493eb526aae15209dddc38fa3fb369
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-16 13:21:38 -0800

    funciton circle

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,26 @@
-// function greet(user) {
-//     // console.log("Hello " + user);
-//     console.log(`Hello ${user}`);
-// }
+function greet(user, time="day") {
+    // console.log("Hello " + user);
+    console.log(`Good ${time} ${user}`);
+}
 
 // const greet = function (user) {
 //     // console.log("Hello " + user);
 //     console.log(`Hello ${user}`);
 // }
 
-const greet = (user) => {
-    // console.log("Hello " + user);
-    console.log(`Hello ${user}`);
+// const greet = (user) => {
+//     // console.log("Hello " + user);
+//     console.log(`Hello ${user}`);
+// }
+
+greet("Roger", "night");
+greet("Ian");
+
+function circleArea(radius) {
+    let area = Math.PI*radius**2;
+    return area;
 }
 
-greet("Roger");
-greet("Ian");
+let r = prompt("Please input a number");
+let area = circleArea(r).toFixed(2);
+alert(`The area of a circle with radius ${r} is ${area}`);
