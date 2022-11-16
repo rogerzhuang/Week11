b77283e967f6923d65649b797f8b559034a909ca
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-16 12:42:18 -0800

    function declaration

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// function greet(user) {
+//     // console.log("Hello " + user);
+//     console.log(`Hello ${user}`);
+// }
+
+// const greet = function (user) {
+//     // console.log("Hello " + user);
+//     console.log(`Hello ${user}`);
+// }
+
+const greet = (user) => {
+    // console.log("Hello " + user);
+    console.log(`Hello ${user}`);
+}
+
+greet("Roger");
+greet("Ian");
